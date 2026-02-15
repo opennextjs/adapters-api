a74294d28da2e451fe5aed3f9fd18830a9efec94
Nicolas Dorseuil <nicolas@gitbook.io>
2026-02-15 01:01:28 +0100

    fix experimental test

--- packages/tests-e2e/tests/experimental/ppr.test.ts
+++ packages/tests-e2e/tests/experimental/ppr.test.ts
@@ -13,12 +13,13 @@ test.describe("PPR", () => {
 
 	test("PPR rsc prefetch request should be cached", async ({ request }) => {
 		const resp = await request.get("/ppr", {
-			headers: { rsc: "1", "next-router-prefetch": "1" },
+			headers: { rsc: "1", "next-router-prefetch": "1", "next-router-segment-prefetch": "/_tree" },
 		});
 		expect(resp.status()).toEqual(200);
 		const headers = resp.headers();
-		expect(headers["x-nextjs-postponed"]).toEqual("1");
-		expect(headers["x-nextjs-cache"]).toEqual("HIT");
-		expect(headers["cache-control"]).toEqual("s-maxage=31536000");
+		expect(headers["x-nextjs-postponed"]).toEqual("2");
+		expect(headers["x-nextjs-prerender"]).toEqual("1");
+		expect(headers["x-opennext-cache"]).toEqual("HIT");
+		expect(headers["cache-control"]).toEqual("s-maxage=31536000, stale-while-revalidate=2592000");
 	});
 });
