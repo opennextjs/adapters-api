6fad0c6038ca26edbbda6f3fd3d8ef150063ba91
Nicolas Dorseuil <nicolas@gitbook.io>
2026-06-27 21:36:43 +0200

    feat: add default overrides for AWS adapter and integrate Cloudflare specific overrides

--- packages/aws/src/adapter.ts
+++ packages/aws/src/adapter.ts
@@ -12,4 +12,29 @@ export default buildAdapter((_config, buildOpts: BuildOptions) => ({
 			return [inlineRouteHandler(updater, outputs, packagePath), externalChunksPlugin(outputs, packagePath)];
 		},
 	},
+	defaultOverrides: {
+		server: {
+			wrapper: "aws-lambda-streaming",
+			converter: "aws-apigw-v2",
+			incrementalCache: "s3",
+			tagCache: "dynamodb",
+			queue: "sqs",
+		},
+		revalidation: {
+			wrapper: "aws-lambda",
+			converter: "sqs-revalidate",
+		},
+		imageOptimization: {
+			wrapper: "aws-lambda",
+			converter: "aws-apigw-v2",
+			imageLoader: "s3",
+		},
+		warmer: {
+			wrapper: "aws-lambda",
+		},
+		tagCache: {
+			wrapper: "aws-lambda",
+			tagCache: "dynamodb",
+		},
+	},
 }));
